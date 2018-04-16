f296156904815f75ece0928ff9d1a710d9860779
Katie O'Shea <k8ie.oshea@gmail.com>
2018-04-16 14:15:25 -0400

    deletecrud

--- restfultasksCRUD/server/config/routes.js
+++
@@ -1,9 +0,0 @@
-let TaskController = require("../controllers/TaskController.js");
-
-module.exports = function(app){
-    app.get("/tasks", TaskController.all);
-    app.post("/tasks", TaskController.create);
-    app.get("/tasks/:id", TaskController.show);
-    app.put("/tasks/:id", TaskController.edit);
-    app.delete("/tasks/:id", TaskController.delete);    
-}
--- restfultasksCRUD/server/models/Task.js
+++
@@ -1,8 +0,0 @@
-let mongoose = require('mongoose');
-
-mongoose.model("Task", new mongoose.Schema({
-    title: { type: String, required: true, minlength: 1, maxlength: 255},
-    description: { type: String, default: "", minlength: 1, maxlength: 255},
-    completed: { type: Boolean, default: false}
-}, {timestamps: true} 
-));
